b5261f3fce275428cb9cce8379d7108a9de49cda
Andre Grossi <info@andregrossi.com>
2020-03-15 06:37:37 -0300

    Create ProductList component and styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,75 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-under-armour-charged-skyline/37/B78-4579-937/B78-4579-937_zoom1.jpg"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-under-armour-charged-skyline/37/B78-4579-937/B78-4579-937_zoom1.jpg"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-under-armour-charged-skyline/37/B78-4579-937/B78-4579-937_zoom1.jpg"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-under-armour-charged-skyline/37/B78-4579-937/B78-4579-937_zoom1.jpg"
+          alt=""
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
